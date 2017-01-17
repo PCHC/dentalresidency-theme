b70c22aeb4f269b7ff1f9448d1467adc2fbb5b0f
Chris Violette <cvioletterun@gmail.com>
2017-01-17 15:00:19 -0500

    Cherry picked from ac530e911a405447546046c290f349bcba808160

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,25 @@ var path = manifest.paths;
 // `config` - Store arbitrary configuration values here.
 var config = manifest.config || {};
 
+var blogpath = config.blogID ? 'blog-'+config.blogID : '';
+
 gulp.task('styles', function(){
   return gulp.src(['custom/scss/*.scss'])
-    .pipe(gulp.dest(path.dist + 'blog-' + config.blogID + '/scss'));
+    .pipe(gulp.dest(path.dist + blogpath + '/scss'));
+});
+
+gulp.task('images', function(){
+  return gulp.src(['custom/images/**/*'])
+    .pipe(gulp.dest(path.dist + blogpath + '/images'));
 });
 
 gulp.task('watch', function(){
   gulp.watch([path.source + 'scss/**/*'], ['styles']);
+  gulp.watch([path.source + 'images/**/*'], ['images']);
 });
 
 gulp.task('default', function(callback){
   runSequence('styles',
+              'images',
               callback);
 });

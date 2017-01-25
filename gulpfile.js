0d1d2badabc37f9911a0a647d2fbaf6d81c799e7
Chris Violette <cvioletterun@gmail.com>
2017-01-25 11:26:27 -0500

    Added particles under blog-number

--- gulpfile.js
+++ gulpfile.js
@@ -22,13 +22,20 @@ gulp.task('images', function(){
     .pipe(gulp.dest(path.dist + blogpath + '/images'));
 });
 
+gulp.task('particles', function(){
+  return gulp.src(['custom/particles/**/*'])
+    .pipe(gulp.dest(path.dist + blogpath + '/particles'));
+});
+
 gulp.task('watch', function(){
   gulp.watch([path.source + 'scss/**/*'], ['styles']);
   gulp.watch([path.source + 'images/**/*'], ['images']);
+  gulp.watch([path.source + 'particles/**/*'], ['particles']);
 });
 
 gulp.task('default', function(callback){
   runSequence('styles',
               'images',
+              'particles',
               callback);
 });
